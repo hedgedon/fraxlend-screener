18b78cbc0c0676e32a344b6e02e189a64c39e9b6
don <donpark@pm.me>
2022-11-16 09:08:35 -0600

    events without filter

--- cmd/fraxlend-screener/thirdweb_test.go
+++ cmd/fraxlend-screener/thirdweb_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/joho/godotenv"
 	"github.com/thirdweb-dev/go-sdk/v2/thirdweb"
@@ -38,7 +39,7 @@ func TestContractEvent(t *testing.T) {
 	}
 }
 
-func TestFraxlendContractEvents(t *testing.T) {
+func TestFraxlendContractEvents_AddCollateral(t *testing.T) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -48,26 +49,42 @@ func TestFraxlendContractEvents(t *testing.T) {
 		t.Errorf("error: %v \n", err)
 	}
 	contract, err := sdk.GetContractFromAbi(FXS_FRAX_POOL, FRAXLEND_PAIR_ABI)
-
-	// First we define a filter to only get AddCollateral events where the "_borrower" address is "0x..."
-	// Note that you can only add filters for indexed parameters on events
 	filters := map[string]interface{}{
-		"_borrower": common.HexToAddress(os.Getenv("MY_ADDRESS")),
+		"_borrower": common.HexToAddress("0xd1F2739ad714045BE6146915275D0a2B822Ec1CC"),
 	}
 	queryOptions := thirdweb.EventQueryOptions{
 		Filters: filters,
 	}
-	// Now we can query for the Transfer events
-	collateralEvents, _ := contract.Events.GetEvents(context.Background(), "AddCollateral", queryOptions)
+	events, _ := contract.Events.GetEvents(context.Background(), "AddCollateral", queryOptions)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if len(collateralEvents) <= 0 {
+	if len(events) <= 0 {
 		t.Logf("no events")
 	}
-
-	for i, e := range collateralEvents {
+	for i, e := range events {
 		t.Logf("%v) Event: %v. tx: %v", i, e.EventName, e.Transaction.TxHash)
+	}
+}
+
+func TestFraxlendContractEvents_BorrowAsset(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	sdk, err := thirdweb.NewThirdwebSDK("mainnet", nil)
+	if err != nil {
+		t.Errorf("error: %v \n", err)
+	}
+	contract, err := sdk.GetContractFromAbi(FXS_FRAX_POOL, FRAXLEND_PAIR_ABI)
+	//filters := map[string]interface{}{
+	//	"_borrower": common.HexToAddress("0xd1F2739ad714045BE6146915275D0a2B822Ec1CC"),
+	//}
+	events, _ := contract.Events.GetEvents(context.Background(), "BorrowAsset", thirdweb.EventQueryOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, e := range events {
+		fmt.Printf("%#v\n", e)
 	}
 }
